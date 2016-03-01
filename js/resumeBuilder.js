4b5fd7c02a3474c5cba6db4a9dd284abf0c4f81c
Yannick Daly <yannickdaly@gmail.com>
2016-02-29 22:52:11 -0500

    Added bio classes to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,34 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name":"Yannick Daly",
+	"role":"Front-End Web Developer",
+	"contact": "yannickdaly@gmail.com",
+	"picture": "images/profile.jpg",
+	"welcome": "I'm a Miami-native interested in front-end web development and creating easy and responsive web apps with awesome UI.",
+	"skills": "My skills include Javascript!"
+};
+
+var education = {
+	"schools": [
+	{
+		"name":"University of Florida",
+		"gradYear":"2012",
+		"major":"Anthropology",
+		"city:":"Gainesville, FL"
+	}
+
+  ]
+}
+
+
+
+var work ={
+
+	"employers" : [ 
+    {
+	   "name" = "Visual Data Solutions",
+	   "yearsWorked" = "1.5",
+	   "position" = "Quality Assurance Assistant",
+	   "city" = "Miami, FL"
+	   
+	}
+
